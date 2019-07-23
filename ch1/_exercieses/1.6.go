71bbcd9ea72503cc326b1d671e04d51ba3e73c51
Andrew Babichev <andrew.babichev@gmail.com>
2019-07-23 12:17:26 +0300

    1.6

---
+++ ch1/_exercieses/1.6.go
@@ -0,0 +1,11 @@
+package main
+
+// I don't know how to share packages yet, so please use the next command to run
+// $ go run 1.12.go lissajous.go > out.gif
+import (
+	"os"
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
