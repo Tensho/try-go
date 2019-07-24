424cd4421b4dea45229abfcafbb5594de92f071f
Andrew Babichev <andrew.babichev@gmail.com>
2019-07-24 18:33:04 +0300

    2.1

---
+++ ch2/_exercieses/2.1/main.go
@@ -0,0 +1,27 @@
+// $ go run -pkgdir tempconv main.go 273.15
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"./tempconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+      t, err := strconv.ParseFloat(arg, 64)
+      if err != nil {
+          fmt.Fprintf(os.Stderr, "%v\n", err)
+          os.Exit(1)
+      }
+      c := tempconv.Celsius(t)
+      f := tempconv.Fahrenheit(t)
+      k := tempconv.Kelvin(t)
+      fmt.Printf("%s\t%s\t%s\n", c, tempconv.CToF(c), tempconv.CToK(c))
+      fmt.Printf("%s\t%s\t%s\n", f, tempconv.FToC(f), tempconv.FToK(f))
+      fmt.Printf("%s\t%s\t%s\n", k, tempconv.KToC(k), tempconv.KToF(k))
+  }
+}
